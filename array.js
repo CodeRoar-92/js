d35de99d415e793a3c7afef2d6c783560f5f1eb3
CodeRoar-92 <160207296+CodeRoar-92@users.noreply.github.com>
2024-03-21 11:45:46 +0500

    array of ja

---
+++ array.js
@@ -0,0 +1,21 @@
+const arr1 = ["Abdullah","Rajab","Afaq"];
+const arr2 = ["Muhammad Ali","Ahmad Raza"];
+//console.log(arr1.concat(arr2));
+
+const arr3 = ["Abdur Rehman","waleed ","Taimoor"]
+//Use of Spread operator
+//console.log([...arr1,...arr2])
+//console.log([...arr1,...arr2,...arr3])
+let numb =[1,2,3,4,[5,6,7],[8,9,[10,11,12,[13,14,15]]]]
+//console.log(numb.flat(Infinity));If we have more arrays in one single array then use it.
+//console.log(Array.isArray("Abdullah"));
+let name =["Abdullah"]
+//console.log(Array.isArray(name));//To check that it is array or not
+
+// arr1.push("Bulbul");
+// console.log(arr1);
+// arr1.pop("Bulbul");
+// console.log(arr1)
+
+arr2.shift("Muhammad Ali");
+////console.log(arr2)
